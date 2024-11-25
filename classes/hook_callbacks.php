bd7de81f7c43668d67dcd933551e1ff3225498cd
p8nut <jeremy.andrey.90@gmail.com>
2024-11-25 17:09:55 +0100

    fix hooks

---
+++ classes/hook_callbacks.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_abconfig;
+
+/**
+ * Hook callbacks for tool_abconfig.
+ *
+ * @package   tool_abconfig
+ * @author    Benjamin Walker (benjaminwalker@catalyst-au.net)
+ * @copyright 2024 Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class hook_callbacks {
+
+    /**
+     * Runs before HTTP headers.
+     *
+     * @param \core\hook\output\before_http_headers $hook
+     */
+    public static function before_http_headers(\core\hook\output\before_http_headers $hook): void {
+        if(!empty(\local_sentry\sentry::get_config('tracking_javascript'))) {
+            return \local_sentry\sentry::get_js_loader_script_html();
+        }
+        return '';
+    }
+
+    /**
+     * Runs before HTTP footers.
+     *
+     * @param \core\hook\output\before_footer_html_generation $hook
+     */
+    public static function before_footer_html_generation(\core\hook\output\before_footer_html_generation $hook): void {
+        \local_sentry\sentry::finish_main_transaction();
+    }
+}
