d26b8aa8cb4114939f5cd1ee48c43ee6e96d55c8
p8nut <jeremy.andrey.90@gmail.com>
2024-11-26 15:17:12 +0100

    fix rename namespace to local_sentry

--- classes/hook_callbacks.php
+++ classes/hook_callbacks.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace tool_abconfig;
+namespace local_sentry;
 
 /**
  * Hook callbacks for tool_abconfig.
