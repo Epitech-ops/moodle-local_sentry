7c3292ae4502d4f4f90fb942dc8bb6915fcec3e4
p8nut <jeremy.andrey.90@gmail.com>
2025-06-12 12:32:06 +0200

    fix admin string name

--- settings.php
+++ settings.php
@@ -89,24 +89,21 @@ if ($hassiteconfig) {
             'local_sentry/sample_rate',
             new lang_string('sample_rate', 'local_sentry'),
             new lang_string('sample_rate_desc', 'local_sentry'),
-            1,
-            PARAM_FLOAT
+            '1'
         ));
         // configure traces_sample_rate
         $settingspage->add(new admin_setting_configtext(
             'local_sentry/traces_sample_rate',
             new lang_string('traces_sample_rate', 'local_sentry'),
             new lang_string('traces_sample_rate_desc', 'local_sentry'),
-            1,
-            PARAM_FLOAT
+            '1'
         ));
         // configure profiles_sample_rate
         $settingspage->add(new admin_setting_configtext(
             'local_sentry/profiles_sample_rate',
             new lang_string('profiles_sample_rate', 'local_sentry'),
             new lang_string('profiles_sample_rate_desc', 'local_sentry'),
-            1,
-            PARAM_FLOAT
+            '1'
         ));
 
     }
